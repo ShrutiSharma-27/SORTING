33a80578b7b3b0c093a5de4de294127b8762667a
Shruti Sharma <53565103+ShrutiSharma-27@users.noreply.github.com>
2023-06-28 17:40:32 +0530

    Add files via upload

---
+++ Selection_sort.java
@@ -0,0 +1,49 @@
+//Selection sort : select an element and put it at its right position.
+//Can do either way : pick maximum or minimum element
+//Not a stable algorithm
+//Use case : it performs well with small lists/arrays
+
+import java.util.Arrays;
+
+public class Selection_sort {
+    public static void main(String[] args) {
+        int[] test_arr = {100,78,-67,90,-1,2,11,3,0};
+        SelectionSort(test_arr);
+        System.out.println(Arrays.toString(test_arr));
+    }
+
+    static void swap(int[] arr, int index1, int index2){
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+
+    static int Max(int[] arr, int tillIndex){
+        int max = arr[0];
+        int maxIndex=0;
+        for (int j = 0 ; j<=tillIndex ; j++) {//till n-i-1 index
+            if (arr[j] > max) {
+                max = arr[j];
+                maxIndex = j;
+            }
+        }
+        return maxIndex ;
+    }
+
+    static void SelectionSort(int[] arr){
+        int n = arr.length ;
+        for (int i=0 ; i<n-1 ; i++){
+            int last = n-i-1; //last index in unsorted array
+            swap(arr, Max(arr, last), last);
+        }
+    }
+}
+
+//time complexity is O(n^2) : for both worst and best case
+//step 1 : n-1 comparisons (finding max element)
+//step 2 : n-2 comparisons (length of unsorted array = n-1 so comparisons needed to find max are n-2)
+//           .
+//           .
+//           1 comparison
+//           0 comparison
+// total comparisons = sum of above and hence give us O(n^2) time complexity
